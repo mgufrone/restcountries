747bfc1df0dfd81f575e03a570e9549fee5ad9c2
Mochamad Gufron <mgufronefendi@gmail.com>
2020-11-25 20:26:43 +0700

    update example

--- example/restcountries_example.dart
+++ example/restcountries_example.dart
@@ -1,27 +1,35 @@
+import 'dart:io';
+
 import 'package:restcountries/restcountries.dart';
 
-main() async {
-  var api = RestCountries.setup("YOUR_API_KEY");
+void main() async {
+  var api = RestCountries.setup(Platform.environment['API_KEY']);
   List<Country> countries;
   List<Region> regions;
   List<City> cities;
   // get all countries
   countries = await api.getCountries();
+  print(countries);
   // or you could do this as long as you have called setup for initialization
   countries = await RestCountries.instance.getCountries();
+  print(countries);
   // search country functionality
   // you could also put city and/or region to narrow down the search of the country
-  countries = await api.searchCountry(keyword: "Indo", city: "", region: "");
+  countries = await api.searchCountry(keyword: 'Indo', city: '', region: '');
+  print(countries);
 
   // get all regions of a country. You need to pass the countryCode as it is required from the API
-  regions = await api.getRegions(countryCode: "id");
+  regions = await api.getRegions(countryCode: 'id');
+  print(regions);
 
   // search region
   regions =
-      await api.searchRegion(countryCode: "id", city: "", keyword: "Jawa");
+      await api.searchRegion(countryCode: 'id', city: '', keyword: 'Jawa');
+  print(regions);
 
   // you could get all cities by just filling the countryCode and region name.
   // if you want to narrow it down to search of some city, put the keyword as parameter
   cities = await api.getCities(
-      countryCode: "id", region: "Jawa Timur", keyword: "mal");
+      countryCode: 'id', region: 'Jawa Timur', keyword: 'mal');
+  print(cities);
 }
